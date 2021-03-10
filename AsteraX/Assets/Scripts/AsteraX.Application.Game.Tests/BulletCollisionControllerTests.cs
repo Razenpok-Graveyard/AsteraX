497895e12104b7c8865742dafcc573f3c5dd7b0a
Andrei Andreev <Razenpok@outlook.com>
2021-03-11 02:59:23 +0300

    Add next level starting when all asteroids are destroyed

--- AsteraX/Assets/Scripts/AsteraX.Application.Game.Tests/BulletCollisionControllerTests.cs
+++ AsteraX/Assets/Scripts/AsteraX.Application.Game.Tests/BulletCollisionControllerTests.cs
@@ -1,7 +1,10 @@
 using System.Linq;
 using AsteraX.Application.Tasks.Game;
+using AsteraX.Application.Tasks.UI;
+using AsteraX.Domain.Game;
 using AsteraX.Infrastructure.Data;
 using Common.Application;
+using Cysharp.Threading.Tasks;
 using FluentAssertions;
 using NUnit.Framework;
 using Razensoft.Functional;
@@ -12,26 +15,43 @@ namespace AsteraX.Application.Game.Tests
     public class BulletCollisionControllerTests
     {
         [Test]
-        public void Colliding_asteroid_with_bullet()
-        {
-            var repository = new GameSessionRepository();
-            var gameSession = repository.Get();
-            var level = new Domain.Game.Level(1, 1, 0);
-            gameSession.StartLevel(level);
-            var asteroidId = gameSession.GetAsteroids().First().Id;
-
-            var taskPublisher = new FakeApplicationTaskPublisher();
-
-            IRequestHandler<Command, Result> sut = new CommandHandler(repository, taskPublisher);
-            var command = new Command
+        public void Colliding_asteroid_with_bullet() => UniTask.ToCoroutine(
+            async () =>
             {
-                AsteroidId = asteroidId
-            };
-            var result = sut.Handle(command);
+                const int asteroidCount = 3;
+                var levelRepository = new LevelRepository();
+                var repository = new GameSessionRepository();
+                var gameSession = repository.Get();
+                var level = new Level(1, 1, 0);
+                gameSession.StartLevel(level);
+                var asteroidId = gameSession.GetAsteroids().First().Id;
 
-            result.IsSuccess.Should().BeTrue();
-            taskPublisher
-                .ShouldContainSingle<DestroyAsteroid>(task => task.Id.Should().Be(asteroidId));
-        }
+                var taskPublisher = new FakeApplicationTaskPublisher();
+
+                IAsyncRequestHandler<Command> sut = new CommandHandler(
+                    levelRepository,
+                    repository,
+                    taskPublisher
+                );
+                var command = new Command
+                {
+                    AsteroidId = asteroidId
+                };
+
+                await sut.Handle(command);
+
+                taskPublisher
+                    .ShouldContainSingle<DestroyAsteroid>(task => task.Id.Should().Be(asteroidId))
+                    .ShouldContainSingle<ShowLoadingScreen>(task =>
+                    {
+                        task.Id.Should().Be(1);
+                        task.Asteroids.Should().Be(3);
+                        task.Children.Should().Be(3);
+                    })
+                    .ShouldContainSingle<SpawnAsteroids>(task => task.Asteroids.Count.Should().Be(asteroidCount))
+                    .ShouldContainSingle<HideLoadingScreen>()
+                    .ShouldContainSingle<UnpauseGame>()
+                    .ShouldContainSingle<EnablePlayerInput>();
+            });
     }
 }
--- AsteraX/Assets/Scripts/AsteraX.Application.Game/Bullets/BulletCollisionController.cs
+++ AsteraX/Assets/Scripts/AsteraX.Application.Game/Bullets/BulletCollisionController.cs
@@ -1,19 +1,25 @@
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading;
 using AsteraX.Application.Game.Asteroids;
 using AsteraX.Application.Tasks.Game;
+using AsteraX.Application.Tasks.UI;
+using AsteraX.Domain.Game;
 using AsteraX.Infrastructure;
 using Common.Application;
-using Razensoft.Functional;
+using Cysharp.Threading.Tasks;
 using UnityEngine;
 using VContainer;
+using static AsteraX.Application.Tasks.Game.SpawnAsteroids;
 
 namespace AsteraX.Application.Game.Bullets
 {
     public class BulletCollisionController : MonoBehaviour
     {
-        private IRequestHandler<Command, Result> _commandHandler;
+        private IAsyncRequestHandler<Command> _commandHandler;
 
         [Inject]
-        public void Construct(IRequestHandler<Command, Result> commandHandler)
+        public void Construct(IAsyncRequestHandler<Command> commandHandler)
         {
             _commandHandler = commandHandler;
         }
@@ -24,38 +30,79 @@ namespace AsteraX.Application.Game.Bullets
             {
                 Destroy(gameObject);
                 var request = new Command {AsteroidId = asteroid.Id};
-                _commandHandler.Handle(request);
+                _commandHandler.Handle(request).Forget();
             }
         }
 
-        public class Command : IRequest<Result>
+        public class Command : IAsyncRequest
         {
             public long AsteroidId { get; set; }
         }
 
-        public class CommandHandler : RequestHandler<Command, Result>
+        public class CommandHandler : AsyncRequestHandler<Command>
         {
+            private readonly ILevelRepository _levelRepository;
             private readonly IGameSessionRepository _gameSessionRepository;
             private readonly IApplicationTaskPublisher _taskPublisher;
 
             public CommandHandler(
+                ILevelRepository levelRepository,
                 IGameSessionRepository gameSessionRepository,
                 IApplicationTaskPublisher taskPublisher)
             {
+                _levelRepository = levelRepository;
                 _gameSessionRepository = gameSessionRepository;
                 _taskPublisher = taskPublisher;
             }
 
-            protected override Result Handle(Command command)
+            protected override async UniTask Handle(Command command, CancellationToken ct)
             {
                 var gameSession = _gameSessionRepository.Get();
                 gameSession.CollideAsteroidWithBullet(command.AsteroidId);
                 _gameSessionRepository.Save();
+
                 _taskPublisher.PublishTask(new DestroyAsteroid
                 {
                     Id = command.AsteroidId
                 });
-                return Result.Success();
+
+                if (gameSession.GetAsteroids().Count > 0)
+                {
+                    return;
+                }
+
+                var level = _levelRepository.GetLevel();
+                gameSession.StartLevel(level);
+                _gameSessionRepository.Save();
+
+                var asteroids = gameSession.GetAsteroids();
+
+                var showLoadingScreen = new ShowLoadingScreen
+                {
+                    Id = (int) level.Id,
+                    Asteroids = level.AsteroidCount,
+                    Children = level.AsteroidChildCount
+                };
+                var spawnAsteroids = new SpawnAsteroids
+                {
+                    Asteroids = ToSpawnAsteroidsDto(asteroids)
+                };
+
+                await _taskPublisher.PublishAsyncTask(showLoadingScreen, ct);
+                _taskPublisher.PublishTask(spawnAsteroids);
+                await _taskPublisher.PublishAsyncTask(new HideLoadingScreen(), ct);
+                _taskPublisher.PublishTask(new UnpauseGame());
+                _taskPublisher.PublishTask(new EnablePlayerInput());
+            }
+
+            private static List<AsteroidDto> ToSpawnAsteroidsDto(IEnumerable<Asteroid> asteroids)
+            {
+                return asteroids.Select(a => new AsteroidDto
+                {
+                    Id = a.Id,
+                    Size = a.Size,
+                    Children = ToSpawnAsteroidsDto(a.Children)
+                }).ToList();
             }
         }
     }
